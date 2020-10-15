8eb887290999f1d0d8a098538254b5bb0406639e
Roman Andreev <anDevRom@gmail.com>
2020-10-15 16:12:58 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const arrForCompare = []
+  let strForCompare = str
+
+  for (let elemOfBracketsConfig of bracketsConfig) {
+    arrForCompare.push(elemOfBracketsConfig.join(""))
+  }
+
+  for (let i = 1; i <= Math.floor(str.length / 2); i++) {
+    arrForCompare.forEach((el) => {
+      if (strForCompare.includes(el)) {
+        strForCompare = strForCompare.split(el).join("")
+      }
+    })
+  }
+
+  if (strForCompare === "") {
+    return true
+  } else {  return false  }
 }
